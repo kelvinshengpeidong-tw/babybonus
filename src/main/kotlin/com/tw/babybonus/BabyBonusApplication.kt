64da90ef4acefa430e2e7148b85d3be609068243
kelvinshengpeidong-tw <296558444+kelvinshengpeidong-tw@users.noreply.github.com>
2026-07-16 17:29:56 +0800

    Initial Spring Boot project for BabyBonus assessment

---
+++ src/main/kotlin/com/tw/babybonus/BabyBonusApplication.kt
@@ -0,0 +1,11 @@
+package com.tw.babybonus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BabyBonusApplication
+
+fun main(args: Array<String>) {
+	runApplication<BabyBonusApplication>(*args)
+}
---
+++ src/test/kotlin/com/tw/babybonus/BabyBonusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tw.babybonus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BabyBonusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
