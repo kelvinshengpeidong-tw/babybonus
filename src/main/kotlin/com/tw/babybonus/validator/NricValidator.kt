84880837b3e74db346a6ac5996902958aa445e41
kelvinshengpeidong-tw <296558444+kelvinshengpeidong-tw@users.noreply.github.com>
2026-07-20 16:07:20 +0800

    refractor: change NricValidator from class to object declaration and update the method to return the normalized nric instead of boolean

--- src/main/kotlin/com/tw/babybonus/validator/NricValidator.kt
+++ src/main/kotlin/com/tw/babybonus/validator/NricValidator.kt
@@ -1,8 +1,10 @@
 package com.tw.babybonus.validator
 
-class NricValidator {
+import com.tw.babybonus.exception.InvalidNricException
 
-    fun isNricFormatValid(nric: String): Boolean {
+object NricValidator {
+
+    fun formatAndValidateNric(nric: String): String {
         //remove any white spaces before/after the NRIC and convert to uppercase
         val normalizedNric = nric.trim().uppercase()
 
@@ -10,7 +12,11 @@ class NricValidator {
         //1. length = 9
         //2. first and last char are letters
         //3. rest of the 7 chars are digits
-        return normalizedNric.matches(Regex("^[A-Z]\\d{7}[A-Z]$"))
+        if(!normalizedNric.matches(Regex("^[A-Z]\\d{7}[A-Z]$"))) {
+            throw InvalidNricException("NRIC format is invalid!")
+        }
+
+        return normalizedNric
     }
 
 }
--- src/test/kotlin/com/tw/babybonus/validator/NricValidatorTest.kt
+++ src/test/kotlin/com/tw/babybonus/validator/NricValidatorTest.kt
@@ -1,58 +1,76 @@
 package com.tw.babybonus.validator
 
+import com.tw.babybonus.exception.InvalidNricException
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import kotlin.test.assertTrue
-import kotlin.test.assertFalse
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
 
 class NricValidatorTest {
 
-    private val nricValidator = NricValidator()
+    val normalizedValidNric: String = "S1234567D"
 
-    @Test
-    fun `should return true if NRIC format is valid`() {
-        val nric = "S1234567D"
-        assertTrue(nricValidator.isNricFormatValid(nric))
+    @Nested
+    inner class NricFormat {
+
+        @Test
+        fun `should return normalized NRIC if NRIC format is valid`() {
+            val nric = "S1234567D"
+            assertEquals(normalizedValidNric, NricValidator.formatAndValidateNric(nric))
+        }
+
+        @Test
+        fun `should return normalized NRIC when NRIC is lowercase`() {
+            val nric = "s1234567d"
+            assertEquals(normalizedValidNric, NricValidator.formatAndValidateNric(nric))
+        }
+
+        @Test
+        fun `should return normalized NRIC when NRIC has leading and trailing spaces`() {
+            val nric = "    S1234567D  "
+            assertEquals(normalizedValidNric, NricValidator.formatAndValidateNric(nric))
+        }
+
+        @Test
+        fun `should throw exception when NRIC length is invalid`() {
+            val nric = "S12345D"
+            assertThrows<InvalidNricException>{
+                NricValidator.formatAndValidateNric(nric)
+            }
+        }
+
+        @Test
+        fun `should throw exception when NRIC starts with a digit`() {
+            val nric = "91234567D"
+            assertThrows<InvalidNricException>{
+                NricValidator.formatAndValidateNric(nric)
+            }
+        }
+
+        @Test
+        fun `should throw exception when NRIC ends with a digit`() {
+            val nric = "S12345679"
+            assertThrows<InvalidNricException>{
+                NricValidator.formatAndValidateNric(nric)
+            }
+        }
+
+        @Test
+        fun `should throw exception when NRIC contains non-digit characters in the middle`() {
+            val nric = "S12AR567D"
+            assertThrows<InvalidNricException>{
+                NricValidator.formatAndValidateNric(nric)
+            }
+        }
+
+        @Test
+        fun `should throw exception when NRIC contains spaces in the middle`() {
+            val nric = "S123 567D"
+            assertThrows<InvalidNricException>{
+                NricValidator.formatAndValidateNric(nric)
+            }
+        }
+
     }
 
-    @Test
-    fun `should return true when NRIC is lowercase`() {
-        val nric = "s1234567d"
-        assertTrue(nricValidator.isNricFormatValid(nric))
-    }
-
-    @Test
-    fun `should return true when NRIC has leading and trailing spaces`() {
-        val nric = "    S1234567D  "
-        assertTrue(nricValidator.isNricFormatValid(nric))
-    }
-
-    @Test
-    fun `should return false when NRIC length is invalid`() {
-        val nric = "S12345D"
-        assertFalse(nricValidator.isNricFormatValid(nric))
-    }
-
-    @Test
-    fun `should return false when NRIC starts with a digit`() {
-        val nric = "91234567D"
-        assertFalse(nricValidator.isNricFormatValid(nric))
-    }
-
-    @Test
-    fun `should return false when NRIC ends with a digit`() {
-        val nric = "S12345679"
-        assertFalse(nricValidator.isNricFormatValid(nric))
-    }
-
-    @Test
-    fun `should return false when NRIC contains non-digit characters in the middle`() {
-        val nric = "S12AR567D"
-        assertFalse(nricValidator.isNricFormatValid(nric))
-    }
-
-    @Test
-    fun `should return false when NRIC contains spaces in the middle`() {
-        val nric = "S123 567D"
-        assertFalse(nricValidator.isNricFormatValid(nric))
-    }
 }
